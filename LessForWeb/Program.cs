5ece717b0bef1a25d9fc5a3cbbe3e6849d53285a
Kristoffer Strube <kristoffer.strube@gmail.com>
2026-04-27 14:15:32 +0000

    Minimal change to see that we can use the API

--- LessForWeb/Program.cs
+++ LessForWeb/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using LessForWeb;
+using KristofferStrube.Blazor.FileSystemAccess;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFileSystemAccessService();
 
 await builder.Build().RunAsync();
